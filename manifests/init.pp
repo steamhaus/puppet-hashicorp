24c76a8646d6ba802a6ffe566d9210341c3928d7
Felix Edelsten <felix@steamhaus.co.uk>
2021-07-15 14:47:34 +0100

    Updating GPG Key ID

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class hashicorp (
 
   gnupg_key { 'hashicorp':
     ensure     => present,
-    key_id     => '51852D87348FFC4C',
+    key_id     => '34365D9472D7468F',
     user       => 'root',
     key_source => 'https://keybase.io/hashicorp/pgp_keys.asc?fingerprint=c874011f0ab405110d02105534365d9472d7468f',
     key_type   => public
